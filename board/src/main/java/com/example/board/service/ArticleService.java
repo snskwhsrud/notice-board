ff45e44f84826d3088d1ffc3867cfa771bf0b199
snskwhsrud <wogur0309@naver.com>
2023-11-21 23:07:57 +0900

    #19 뷰 템플릿에 페이지네이션 기능 반영
    이거는 복사를 해서 좀 많이 나왔음 좀 수정을 해야함

--- board/src/main/java/com/example/board/service/ArticleService.java
+++ board/src/main/java/com/example/board/service/ArticleService.java
@@ -31,10 +31,8 @@ public class ArticleService {
         return switch (searchType) {
             case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
             case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
-            case ID ->
-                    articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
-            case NICKNAME ->
-                    articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
             case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
         };
     }
@@ -52,21 +50,21 @@ public class ArticleService {
 
     public void updateArticle(ArticleDto dto) {
         try {
-
             Article article = articleRepository.getReferenceById(dto.id());
-            if (dto.title() != null) {
-                article.setTitle(dto.title());
-            }
-            if (dto.content() != null) {
-                article.setContent(dto.content());
-            }
+            if (dto.title() != null) { article.setTitle(dto.title()); }
+            if (dto.content() != null) { article.setContent(dto.content()); }
             article.setHashtag(dto.hashtag());
         } catch (EntityNotFoundException e) {
             log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
         }
     }
-        public void deleteArticle ( long articleId){
-            articleRepository.deleteById(articleId);
-        }
 
+    public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
+
+    public long getArticleCount() {
+        return articleRepository.count();
+    }
+
+}
