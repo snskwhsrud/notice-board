b2da0e1d90a80f949b7aca3e40d9048ebca97607
snskwhsrud <wogur0309@naver.com>
2023-11-30 22:33:43 +0900

    #37- 해시태그
    고도화 작업중

--- board/src/main/java/com/example/board/controller/ArticleCommentController.java
+++ board/src/main/java/com/example/board/controller/ArticleCommentController.java
@@ -24,6 +24,7 @@ public class ArticleCommentController {
     ) {
         articleCommentService.saveArticleComment(articleCommentRequest.toDto(boardPrincipal.toDto()));
 
+
         return "redirect:/articles/" + articleCommentRequest.articleId();
     }
 
