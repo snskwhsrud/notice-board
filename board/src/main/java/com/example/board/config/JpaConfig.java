5fc274a8568664ad8d29844f058e7626f6770f5f
snskwhsrud <wogur0309@naver.com>
2023-12-05 17:53:16 +0900

    #58 프로젝트 최종 수정완료
    이제는 혼자만들어봐야함

--- board/src/main/java/com/example/board/config/JpaConfig.java
+++ board/src/main/java/com/example/board/config/JpaConfig.java
@@ -1,6 +1,5 @@
 package com.example.board.config;
 
-
 import com.example.board.dto.security.BoardPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
